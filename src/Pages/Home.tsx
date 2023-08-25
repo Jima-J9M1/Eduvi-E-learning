f45bb1689efc228302d38fded60bd3430d70bc9e
Your Nameyigermal <yigoya7@gmail.com>
2023-08-25 11:01:53 +0300

    Adding similarCourses ui

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,14 @@
+import AdsCard from "../Components/Course/AdsCard";
+import vite from '../../public/vite.svg';
+import SimilarCourses from "../Components/Course/SimilarCourses";
+
 const Home = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-    </>
+    <div className="md:p-16 max-md:p-10 flex flex-col justify-center align-center h-screen bg-gray-100">
+      {/* add other homepage components here */}
+      <AdsCard image={vite} adName="Vite Ad" /> {/*move this component to course detail page*/}
+      <SimilarCourses/>
+    </div>
   );
 };
 
