8f59a726d4633edf7a4042876bcf683a8dafc6dd
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-09-08 20:35:24 +0300

    Update Courselist.tsx of the categories dropdown and add navelink and change the visit button

--- src/Components/Common/Courselist.tsx
+++ src/Components/Common/Courselist.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 import CourseCard from "./CourseCard";
 import Button from '@mui/material/Button';
 import NativeSelectDemo from "./categories";
@@ -17,21 +18,30 @@ const CourseList: React.FC = () => {
   return (
     <div className="mt-10">
       <div className="flex sm:justify-start lg:justify-end mb-catb-pos mr-cat-pos ml-cat-pos">
-        <div className="grid grid-cols-2 gap-4 mt-catb-pos">
-          <NativeSelectDemo />
-          <Button variant="contained" disableElevation>
-            View Courses
-          </Button>
+        <div className="flex ">
+          <div className = " pt-2 pr-10 font-poppins text-xl font-medium leading-6 tracking-tight text-left text-gray-800">
+          <select>
+            <option selected disabled>Categories</option>
+            <option value="Front end">Front end</option>
+            <option value="Back end">Back end</option>
+            
+          </select>  
+            
+          </div>
+          <VisitButton text = "Visit More Courses" />
         </div>
       </div>
       <div className="grid border-t-2 border-solid border-gray-200 m-catb-pos sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center g-10 bg-list-back pt-20">
         {courses.map((course) => (
-          <CourseCard
-          id={course.id}
-            key={course.id}
-            courseType={course.courseType}
-            image={course.image}
-          />
+                
+            <NavLink to={`/${course.id}`}>
+            <CourseCard
+              id={course.id}
+              key={course.id}
+              courseType={course.courseType}
+              image={course.image} 
+            />
+          </NavLink>
         ))}
       </div>
     </div>
