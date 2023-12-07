56b6d473c1ff6bfb81ac47cf3c0e21b5b2ce8013
seid yesuf <seidyesuf750@gmail.com>
2023-12-07 04:30:31 -0800

    application form

---
+++ src/Api/internApplication.ts
@@ -0,0 +1,33 @@
+
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+type coustemprops ={
+    data:{}
+    onSuccess:(test)=>unknown
+    onError:(test)=>unknown
+}
+
+
+
+const createApplication = async (data)=> {
+    console.log(data.resume[0]);
+    
+    const response =  await axios.post("https://portal.bluemarkcollege.com/apply/aparentship",{
+        data:data
+    }).then((res)=>res)
+    console.log(response);
+    
+    return response
+  }
+
+  export default function CoustemCreateapp({onError,onSuccess,data}:coustemprops){  
+    return useQuery(['posts',data],()=>createApplication(data),{
+        onSuccess,
+        onError,
+        select:(data)=>{
+         return data    
+        },
+        enabled:false
+    })
+    }
