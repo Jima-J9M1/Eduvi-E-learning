1ec46b4a1b947ee4ed4bcc76b79d99819ac471d4
seid yesuf <seidyesuf750@gmail.com>
2023-12-08 22:29:48 -0800

    apllication functionaltiy

--- src/Api/internApplication.ts
+++ src/Api/internApplication.ts
@@ -3,22 +3,27 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
 type coustemprops ={
-    data:{}
+    formData: globalThis.FormData
     onSuccess:(test)=>unknown
     onError:(test)=>unknown
 }
 
 
 
-const createApplication = async (data)=> {
-    const response =  await axios.post("https://portal.bluemarkcollege.com/apply/aparentship",{
-        data:data
+const createApplication = async (formData)=> {
+    for (let obj of formData) {
+        console.log(obj);
+      }
+    const response =  await axios.post("https://portal.bluemarkcollege.com/apply/aparentship",formData,{
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
     }).then((res)=>res)
     return response
   }
 
-  export default function CoustemCreateapp({onError,onSuccess,data}:coustemprops){  
-    return useQuery(['posts',data],()=>createApplication(data),{
+  export default function CoustemCreateapp({onError,onSuccess,formData}:coustemprops){  
+    return useQuery(['posts',formData],()=>createApplication(formData),{
         onSuccess,
         onError,
         select:(data)=>{
